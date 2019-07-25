9a0730ae22330e619133826e0a95a00380d141e2
Kevin <nelson67@stolaf.edu>
2019-07-24 20:09:44 -0500

    adds course model

---
+++ db/migrate/20190725000711_drop_course.rb
@@ -0,0 +1,5 @@
+class DropCourse < ActiveRecord::Migration[6.0]
+  def change
+    drop_table :courses
+  end
+end
---
+++ db/migrate/20190725000907_add_all_columns_to_course.rb
@@ -0,0 +1,4 @@
+class AddAllColumnsToCourse < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
