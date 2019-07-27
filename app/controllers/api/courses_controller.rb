c156d2131b29a36b1cf364aa4299310a529109df
Kevin <nelson67@stolaf.edu>
2019-07-27 04:37:24 -0500

    adds index file for courses

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -2,23 +2,9 @@ class Api::CoursesController < ApplicationController
   def index
     @courses = Course.all
 
-    if params[:query]
-      temp_courses = courses
-      courses = []
-      temp_courses.each do |course|
-        next if course[params[:query]] == 'false'
-
-        @courses << course[params[:query]]
-      end
-    end
-
     if params[:department]
       input_department = params[:department]
       @courses = @courses.all.where(department: input_department)
-    end
-
-    if params[:keyword]
-      input_keyword = params[:keyword]
     end
 
     if params[:level]
@@ -28,38 +14,7 @@ class Api::CoursesController < ApplicationController
 
     if params[:days]
       input_days = params[:days]
-      @courses = @courses.map do |course|
-        offerings = course['offerings'].to_s.gsub(/[\"\[\]]/, '')
-        starts = offerings.scan(/start=>(\w+:\w+)/)
-        ends = offerings.scan(/end=>(\w+:\w+)/)
-        times = []
-
-        starts.length.times do |idx|
-          times << "#{starts[idx]} - #{ends[idx]}".to_s.gsub(/[\"\]\[]/, '')
-        end
-
-        times = times.to_s.gsub(/[\"\[\]]/, '')
-        days = offerings.scan(/day=>(\w+)/).to_s.gsub(/[\"\[\]]/, '')
-        days = days.gsub(',', '')
-
-        locations = offerings.scan(/location=>(\w+\s\d+)/)
-        locations = locations.uniq
-        locations = locations.to_s
-        locations = locations.gsub(/[\"\[\]]/, '')
-
-        # if days == 'MWFT'
-        #   days = 'MWFTh'
-        # end
-
-        num_of_days = days.split(',').length
-        # next unless locations.include?(',')
-        {
-          course: course,
-          days: days,
-          times: times,
-          location: locations
-        }
-      end
+      @courses = @courses.all.where(days: input_days)
     end
 
     if params[:gereqs]
@@ -69,6 +24,6 @@ class Api::CoursesController < ApplicationController
       end
     end
 
-    render json: { count: @courses.length, courses: @courses }
+    render 'index.json.jb'
   end
 end
