1107e806a606d18a8a9022d94b094c44f017c9b3
Kevin <nelson67@stolaf.edu>
2019-07-29 15:50:47 -0500

    adds user login

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::UsersController < ApplicationController
   def create
     user = User.new(
+      name: params[:name],
       username: params[:username],
       email: params[:email],
       password: params[:password],
@@ -11,6 +12,14 @@ class Api::UsersController < ApplicationController
       render json: { message: 'User created successfully' }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    if current_user
+      render json: { message: current_user }
+    else
+      render json: { message: 'please log in' }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     get '/courses' => 'courses#index'
 
+    get '/users/:id' => 'users#show'
     post '/users' => 'users#create'
 
     post '/sessions' => 'sessions#create'
