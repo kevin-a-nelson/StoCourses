4ab717fa2bd992c1fb9bf2d5c4689c8db00b1cd6
Kevin <nelson67@stolaf.edu>
2019-07-30 02:08:08 -0500

    adds user_id to planner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_070336) do
+ActiveRecord::Schema.define(version: 2019_07_30_070657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_07_30_070336) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "year"
     t.integer "semester"
+    t.integer "user_id"
   end
 
   create_table "selectors", force: :cascade do |t|
