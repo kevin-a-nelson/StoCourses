61dea14f6b6a9c4befaa36bb0c715f95b0346bb9
Kevin <nelson67@stolaf.edu>
2019-08-17 21:32:25 -0500

    converts nil nums to 0

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,110 +10,32 @@ class Course < ApplicationRecord
 
   validates :clbid, uniqueness: true
 
-  def index_elem_hash(attribute)
-    attr_hash = {}
-    split_attr = attribute.split(', ')
-    split_attr.each.with_index do |value, i|
-      attr_hash[i] = value
-    end
-    attr_hash
-  end
-
-  def dept_num_section
-    {
-      'dept': department,
-      'num_sec': "#{number} #{section}"
-    }
-  end
-
-  def formated_times
-    return { '0': '---' } if times == ''
-
-    index_elem_hash(times)
-  end
-
-  def pretty_name
-    name.gsub('/', '/ ')
+  def dept_num_sec
+    "#{department} #{number}#{section}"
   end
 
   def num_of_ges
     gereqs.split(', ').count
   end
 
-  def pretty_gereqs
-    return { '0': '---' } if gereqs == ''
-
-    gereqs_hash = {}
-    gereqs.split(',').each.with_index do |ge, i|
-      gereqs_hash[i] = ge
-    end
-    gereqs_hash
+  def num_reviews
+    profs[0] ? profs[0]['num_ratings'].to_f : 0
   end
 
-  def pretty_instructors
-    index_elem_hash(instructors)
+  def difficulty_int
+    profs[0] ? profs[0]['difficulty'].to_f : 0
   end
 
-  def pretty_location
-    return '---' if location == ''
-
-    location.split(',').uniq[0]
-  end
-
-  def pretty_section
-    section || ''
-  end
-
-  def pretty_days
-    return '---' if days == ''
-
-    days
-  end
-
-  def pretty_description
-    return '---' if description == ''
-
-    description
-  end
-
-  def pretty_notes
-    return '---' if notes == ''
-
-    notes
-  end
-
-  def semester_num_to_name
-    case semester
-    when 1 then 'fall'
-    when 2 then 'interim'
-    when 3 then 'spring'
-    when 4 then 'summer session 1'
-    when 5 then 'summer session 2'
-    end
-  end
-
-  def num_ratings
-    return profs[0]['num_ratings'].to_i if profs[0] && profs[0]['num_ratings'] != 'N/A'
-
-    0
-  end
-
-  def difficulty
-    return profs[0]['difficulty'].to_f if profs[0] && profs[0]['difficulty'] != 'N/A'
-
-    '--'
-  end
-
-  def rating
-    return profs[0]['rating'].to_f if profs[0] && profs[0]['rating'] != 'N/A'
-
-    '--'
+  def rating_int
+    profs[0] ? profs[0]['rating'].to_f : 0
   end
 
   def rate_my_prof_url
-    return "https://www.ratemyprofessors.com/ShowRatings.jsp?tid=#{profs[0]['tid']}" if profs[0]
+    "https://www.ratemyprofessors.com/ShowRatings.jsp?tid=#{profs[0]['tid']}" if profs[0]
+  end
 
-    '--'
+  def has_prereqs
+    prerequisites == ('f' || '') ? 'no' : 'yes'
   end
 
   def seats
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
+    origins 'localhost:8080', 'localhost:17610', 'localhost:35247'
 
     resource '*',
       headers: :any,
